57f04be8f78470c3c4d282bc5611c0bb7fb9efa4
Scott Flynn-Elhoffer <57599533+smfxfc@users.noreply.github.com>
2022-07-08 17:45:34 -0700

    added targets and launch arrow

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 public class Bird : MonoBehaviour
 {
     private Vector3 _initialPosition;
+    private bool _birdLaunched;
+    private float _timeDormant;
     
     // create adjustable launchPower variable in Unity
     [SerializeField] private float _launchPower = 500;
@@ -15,9 +17,26 @@ public class Bird : MonoBehaviour
     }
 
     private void Update()
-    // check for bird position and reset if necessary
     {
-        if (transform.position.y > 10)
+        GetComponent<LineRenderer>().SetPosition(0, _initialPosition); // starting point of line
+        GetComponent<LineRenderer>().SetPosition(1, transform.position); // ending point of line
+
+
+        // if bird was launched and has stopped, start reset timer
+        if (_birdLaunched && 
+        GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeDormant += Time.deltaTime;
+        }
+    
+
+        // check for bird position and reset if outside boundaries
+        if (transform.position.y > 10 || 
+            transform.position.y < -10 ||
+            transform.position.x > 10 ||
+            transform.position.x < -10 ||
+            _timeDormant > 3
+            )
         {
             string currentSceneName = SceneManager.GetActiveScene().name;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -28,6 +47,7 @@ public class Bird : MonoBehaviour
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true; // show launch arrow
     }
 
     private void OnMouseUp()
@@ -40,6 +60,11 @@ public class Bird : MonoBehaviour
 
         // set gravity scale
         GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdLaunched = true;
+        
+        // shut off launch arrow
+        GetComponent<LineRenderer>().enabled = false;
+
     }
 
     private void OnMouseDrag() 
